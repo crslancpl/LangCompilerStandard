c688c72af498b195e7f81b0669467cf773b72ef3
AAAAAB <157278795+ericNen@users.noreply.github.com>
2025-04-23 19:39:46 +0800

    4/23 +src +TestingInputFiles
    Add CompilerEntry.cpp to src and First.txt to TestingInputFiles

---
+++ src/CompilerEntry.cpp
@@ -0,0 +1,13 @@
+/*
+* This is the entry of the compiler, where it will only do the
+* job of reading every characters of the input file and pass it
+* to the
+*/
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    return 0;
+}
